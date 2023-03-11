434105100a488d91bac5f4929da45c8b07bdd0a8
rchoorikkadu <rchoorikkadu@nucleara.com>
2023-03-11 22:50:53 +0000

    Initial commit

---
+++ cypress/e2e/duckduckgo.mjs
@@ -0,0 +1,17 @@
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+When("I visit duckduckgo.com", () => {
+  cy.visit("https://duckduckgo.com/");
+});
+
+Then("I should see a search bar", () => {
+  cy.get("input").should(
+    "have.attr",
+    "placeholder",
+    "Search the web without being tracked"
+  );
+  //cy.get('#sample').type('to make the test fail')
+
+  assert.deepEqual({}, {});
+  //assert.deepEqual(true, false)
+});
